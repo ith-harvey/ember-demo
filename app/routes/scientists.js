de9497f7f2bd832fd31fa5cc907d3663fe91a3a6
Ian Harvey <ith.harvey@gmail.com>
2017-08-16 19:03:01 -0500

    can now on submit fire a function

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -2,8 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'
-    ]
-  }
+    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+  },
+  actions: {
+    addPerson(model) {
+      console.log('in addPerson', model.get('name'))
 
+      this.get('model').push(model.get('name'));
+    }
+  }
 });
